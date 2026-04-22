210e884c6b7e8229cfb48e866535d1ced31f9c08
Karelia Zamorano <karelia.zamorano@gmail.com>
2026-04-22 19:16:15 +0200

    ajustes en t1e1 y t2e1

--- ejercicios_semana_9/t1e1peliculas.py
+++ ejercicios_semana_9/t1e1peliculas.py
@@ -7,6 +7,7 @@ duracion = "89"
 premios = True
 
 #2. Muestra la información en la consola
+print("Mi película favorita en español")
 print("El título de la película:", titulo_pelicula)
 print("El director de la película:", director)
 print("El año de lanzamiento fue:", lanzamiento)
@@ -16,16 +17,13 @@ print("¿Tiene premios la película?", premios)
 
 #3. Cambia los valores de las variables a datos en inglés.
 title = "Yes Day"
-director2 = "Miguel Arteta"
-year = "2021"
 gender = "comedy"
-duration = "89"
-awards = True
 
 #4. Vuelve a mostrar la información en la consola.
+print("My favourite film in english")
 print("The film title is:", title)
-print("The film director was:", director2)
-print("The release film year was:", year)
+print("The film director was:", director)
+print("The release film year was:", lanzamiento)
 print("The gender is:", gender)
-print("The film duration in minutes is:", duration)
-print("¿Does it have awards?", awards)
+print("The film duration in minutes is:", duracion)
+print("¿Does it have awards?", premios)
--- ejercicios_semana_9/t2e1calculadora.py
+++ ejercicios_semana_9/t2e1calculadora.py
@@ -1,6 +1,6 @@
 #1Pide a la usuaria dos números
-numero1 = float(input("Escribe un numero "))
-numero2 = float(input("Escribe otro numero "))
+numero1 = float(input("Escribe un número "))
+numero2 = float(input("Escribe otro número "))
 
 #2Muestra el resultado de:
 #Suma
@@ -12,7 +12,7 @@ multiplicacion = numero1 * numero2
 #División
 division = numero1 / numero2
 
-print("Suma: ", suma)
-print("Resta: ", resta)
-print("Multiplicacion: ", multiplicacion)
-print("Division: ", division)
+print("Suma de los números: ", suma)
+print("Resta de los números: ", resta)
+print("Multiplicación de los números: ", multiplicacion)
+print("División de los números: ", division)
