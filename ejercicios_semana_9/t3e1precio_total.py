2c947ef4fca96bb4adb608986be12fb7eb2893c2
Karelia Zamorano <karelia.zamorano@gmail.com>
2026-04-24 12:06:44 +0200

    ajustes en ejercicios semana 9

--- ejercicios_semana_9/t3e1precio_total.py
+++ ejercicios_semana_9/t3e1precio_total.py
@@ -1,26 +1,28 @@
 #Precio total de un producto
-def subtotal_compra(precio_u,cantidad_u):
-    subtotal = precio_u * cantidad_u
+def compra(precio, cantidad):
+    subtotal = precio * cantidad
     return subtotal
 
-def total_descuento(descuento_porcentaje):
-    descuento = subtotal_compra(precio_u,cantidad_u) - ((subtotal_compra(precio_u,cantidad_u)) * (descuento_porcentaje / 100))
-    return descuento
+def descuento(subtotal, descuento_porcentaje):
+    subtotal_descuento = subtotal - (subtotal * descuento_porcentaje / 100)
+    return subtotal_descuento
 
-def total_iva(iva_porcentaje):
-    total = (total_descuento(descuento_porcentaje) + ((total_descuento(descuento_porcentaje) * iva_porcentaje)) / 100)
+def iva(subtotal_descuento, iva_porcentaje):
+    total = subtotal_descuento + (subtotal_descuento * iva_porcentaje / 100)
     return total
 
-#Solicita a la usuaria:
-nombre_producto = input("Escribe el nombre del producto: ")
-precio_u = float(input("Escribe el precio por unidad en euros: "))
-cantidad_u = int(input("Escribe la cantidad de unidades a comprar: "))
-descuento_porcentaje = float(input("Escribe el porcentaje de descuento: "))
-iva_porcentaje = float(input("Escribe el IVA en porcentaje: "))
-
+#Solicita a la usuaria
 #Muestra el resultado final
-subtotal = subtotal_compra(precio_u,cantidad_u)
-descuento = total_descuento(descuento_porcentaje)
-total = total_iva(iva_porcentaje)
-print("Si compra", cantidad_u, nombre_producto, "el total a pagar será:", total, "euros.")
+def total_pagar():
+    producto = input("Escribe el nombre del producto: ")
+    precio = float(input("Escribe el precio por unidad en euros: "))
+    cantidad = int(input("Escribe la cantidad de unidades a comprar: "))
+    descuento_porcentaje = float(input("Escribe el porcentaje de descuento: "))
+    iva_porcentaje = float(input("Escribe el IVA en porcentaje: "))
+    subtotal = compra(precio, cantidad)
+    subtotal_descuento = descuento(subtotal, descuento_porcentaje)
+    total = iva(subtotal_descuento, iva_porcentaje)
+    print("Si compra", cantidad, producto, "el total a pagar será:", total, "euros.")
 
+
+total_pagar()
