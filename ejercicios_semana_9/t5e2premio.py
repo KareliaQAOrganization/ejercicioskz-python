d7ff8e61486ff2de6e8ec5ecf6e7ff023b888348
Karelia Zamorano <karelia.zamorano@gmail.com>
2026-04-27 19:20:43 +0200

    terminado t5e2

--- ejercicios_semana_9/t5e2premio.py
+++ ejercicios_semana_9/t5e2premio.py
@@ -1,20 +1,39 @@
 #Pide a la usuaria 5 veces que introduzca un color.
-#Si la usuaria introduce el color azul, muestra que el premio ha sido conseguido. Si no, dile que pruebe otro color.
+#Si la usuaria introduce el color azul, muestra que el premio ha sido conseguido. 
+#Si introduce otro color, dile que pruebe otro color.
+
+cantidad = 5
+
+def pedir_colores(cantidad):
+    colores = []
+    
+    for i in range(cantidad):
+        color = input("Escribe un color: ")
+        if color == 'azul':
+            colores.append(color)
+            print("PREMIO CONSEGUIDO")
+            break
+        else:
+            print("Prueba con otro color")
+    return colores
+
+colores = pedir_colores(cantidad)
 
 
-#color = print(input("Escribe un color: "))
-
-
-for color in range(5):
-    color = print(input("Escribe un color: "))
-    if color == 'azul':
-        print("El premio ha sido conseguido")
-        break
-    else:
-        print(input("Intenta con otro color: "))
-        continue
-    print("Fin del juego")
 
 
 
-resultado = ganador(premio)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ejercicios_semana_9/zamorano.py
@@ -0,0 +1,65 @@
+#PARTE 1: Pedir datos
+#Pregunta cuántas notas quiere introducir la usuaria
+#Usa un bucle para pedir cada nota
+#Guarda todas las notas en una lista
+
+#PARTE 2: Validación (condicional)
+#Si la usuaria introduce una nota menor que 0 o mayor que 10:
+#NO la guardes
+#Muestra: "Nota inválida"
+#(aquí puedes usar continue si quieres)
+
+#🔹 PARTE 3: Cálculos (operaciones)
+#Con las notas válidas:
+#Calcula la suma total
+#Calcula la media
+
+#🔹 PARTE 4: Resultado (condicional)
+#Si la media ≥ 5 → mostrar "Aprobado"
+#Si la media < 5 → mostrar "Suspendido"
+
+cantidad = int(input("Cuántas notas quiere introducir?: "))
+
+def pedir_notas(cantidad):
+    notas = []
+    total = 0
+    
+    for i in range(cantidad):
+        nota = float(input("Cuál es la nota?: "))
+        if nota >= 0 and nota <= 10:
+            notas.append(nota)
+            total = total + nota
+        else:
+            print("Nota inválida")
+    return notas, total
+
+def calcular_media(notas, total):
+    if len(notas) > 0:
+        return total / len(notas)
+    else:
+        return 0
+
+def resultado(notas, media):
+    if len(notas) == 0:
+        print("No hay notas válidas")
+    else:
+        if media >= 5.0:
+            print("Aprobado")
+        else:
+            print("Suspendido")
+
+notas, total = pedir_notas(cantidad)
+media = calcular_media(notas, total)
+
+print("Las notas introducidas son:", notas)
+print("La suma total es: ", total)
+print("La media de las notas válidas es:", media)
+
+
+
+
+
+#🔹 PARTE 5: Extra (para subir nota 😉)
+#Muestra:
+#Nota más alta
+#Nota más baja
